ba47a685ad0f6a997726a0e8b7acf5aa7331eeb6
ArifOpBD <bdarif921@gmail.com>
2026-04-28 02:40:00 +0600

    Update signals.py

--- signals.py
+++ signals.py
@@ -1,6 +1,11 @@
 import random
 
-PAIRS = ["EUR/USD","GBP/USD","USD/JPY","AUD/USD","USD/CAD"]
+PAIRS = ["EUR/USD OTC", "GBP/USD OTC", "USD/JPY OTC", "AUD/USD OTC", "USD/CAD OTC",
+        "USD/CHF OTC", "NZD/USD OTC", "EUR/GBP OTC", "EUR/JPY OTC", "GBP/JPY OTC",
+        "AUD/JPY OTC", "CAD/JPY OTC", "CHF/JPY OTC", "NZD/JPY OTC",
+        "EUR/AUD OTC", "EUR/CAD OTC", "GBP/AUD OTC", "GBP/CAD OTC",
+        "BTC/USD OTC", "ETH/USD OTC", "XAU/USD OTC", "NAS100 OTC",
+        "SPX500 OTC", "UK100 OTC", "GER30 OTC""]
 
 def get_signal():
     return {
