2898177a7485f917b405098c0e6db6ce64ea0cc5
ArifOpBD <bdarif921@gmail.com>
2026-04-28 02:40:59 +0600

    Update time_engine.py

--- time_engine.py
+++ time_engine.py
@@ -3,9 +3,10 @@ from datetime import datetime, timedelta
 def execution_time():
     return datetime.utcnow() + timedelta(minutes=2)
 
-def countdown(t):
+def countdown(target):
+
     now = datetime.utcnow()
-    sec = int((t - now).total_seconds())
+    sec = int((target - now).total_seconds())
 
     if sec < 0:
         return "00:00"
